f0110c3829b5c536058eeb73a2abf840c071e1e3
Hatim <38246876+Hajdaini@users.noreply.github.com>
2018-08-03 09:52:51 +0200

    Add files via upload

---
+++ Python/Thug-life-maker/README.md
@@ -0,0 +1,23 @@
+# thug-life-maker
+
+## Description : 
+This script can add the "thug life filter" on image with a face. For example :
+<br /><br /><b>before :</b>
+<img src="images/hatim.jpg" width="100">
+<br /><b>after :</b>
+<img src="result/result.JPEG" width="100">
+
+## Prerequisites before running the script :
+
+ 1. Install python 3 
+ 2. Install PIL and opencv 
+	 ```
+	 sudo apt-get -y install python3-pip
+ 	 pip3 install opencv-python pillow
+	```
+
+## How to use it :
+
+```py
+python3 thug_life_maker.py your_image.jpg
+```
---
+++ Python/Thug-life-maker/thug_life_maker.py
@@ -0,0 +1,42 @@
+"""
+@Author : AJDAINI Hatim
+@GitHub : https://github.com/Hajdaini
+"""
+
+from PIL import Image
+import cv2
+import sys
+
+if len(sys.argv) == 2:
+    imagePath = sys.argv[1]
+else:
+    print('Error : you have to run the program like this :\npython3 thug_life_maker.py your_image.jpg')
+    exit(-1)
+
+maskPath = "images/mask.png"
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+image = cv2.imread(imagePath)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(gray_image, 1.15)
+background_image = Image.open(imagePath)
+
+for (x, y, width, height) in faces:
+    cv2.rectangle(image, (x,y), (x+width, y+height), (0, 0, 255), 2)
+    mask = Image.open(maskPath)
+    mask = mask.resize((width, height), Image.ANTIALIAS)# ANTIALIAS to make image smooth
+    offset = (x, y)
+    background_image.paste(mask, offset, mask=mask)# mask= keep transparency
+    
+try:
+    background_image.save('result/result.{}'.format(background_image.format))
+    print('successfully saved in result/result.{}'.format(background_image.format))
+except:
+    print('Failed to saved the image')
+    exit(-1)
+
+try:
+    background_image.show()
+except:
+    print('make sure that you have an image viewer installed in your system')
+    exit(-1)
